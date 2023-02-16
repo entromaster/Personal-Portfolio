1cd6d8a80c232197b70c540b12619a1321b8a3ab
Suman Saurav <sumansaurav0243@gmail.com>
2023-02-17 00:45:06 +0530

    Project Nav-Bar working now

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+  const toRotate = ["Student", "Web Developer", "Tech Enthusiast"];
   const period = 2000;
 
   useEffect(() => {
@@ -69,7 +69,7 @@ export const Banner = () => {
                     <span
                       className="txt-rotate"
                       dataPeriod="1000"
-                      data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'
+                      data-rotate='[ "Student", "Web Developer", "Tech Enthusiast" ]'
                     >
                       <span className="wrap">{text}</span>
                     </span>
